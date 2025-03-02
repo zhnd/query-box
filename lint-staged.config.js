8cfa0e2a7e51d24c0429480d84f04b5d71a45b7e
zhnd <zhufind@yeah.net>
2025-03-02 18:06:16 +0800

    add Husky pre-commit hook and configure lint-staged for automatic linting

---
+++ lint-staged.config.js
@@ -0,0 +1,6 @@
+/* eslint-disable no-undef */
+module.exports = {
+  '*.{js,jsx,mjs,ts,tsx,mts,mdx}': ['prettier --write', 'eslint --fix'],
+  '*.{json,md,css,html,yml,yaml,scss}': ['prettier --write'],
+  '*.rs': ['cargo fmt --'],
+}
