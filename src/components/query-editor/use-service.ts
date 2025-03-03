652dfcf95dc738004cf6ebaefcdaea142007c53d
zhnd <zhufind@yeah.net>
2025-03-03 21:59:13 +0800

    add lodash and @types/lodash, implement debounce get editor value

--- src/components/query-editor/use-service.ts
+++ src/components/query-editor/use-service.ts
@@ -1,25 +1,26 @@
-import { createGraphiQLFetcher } from "@graphiql/toolkit";
+import { createGraphiQLFetcher } from '@graphiql/toolkit'
 import {
   buildClientSchema,
   getIntrospectionQuery,
   GraphQLSchema,
   IntrospectionQuery,
-} from "graphql";
-import { editor, Uri } from "monaco-editor";
-import { initializeMode } from "monaco-graphql/initializeMode";
-import { useEffect, useRef } from "react";
+} from 'graphql'
+import { debounce } from 'lodash'
+import { editor, Uri } from 'monaco-editor'
+import { initializeMode } from 'monaco-graphql/initializeMode'
+import { useEffect, useRef } from 'react'
 
 export function useService() {
-  const editorContainerElementRef = useRef<HTMLDivElement>(null);
-  const editorInstanceRef = useRef<editor.IStandaloneCodeEditor | null>(null);
-  const initializingRef = useRef(false);
+  const editorContainerElementRef = useRef<HTMLDivElement>(null)
+  const editorInstanceRef = useRef<editor.IStandaloneCodeEditor | null>(null)
+  const initializingRef = useRef(false)
 
   const init = async () => {
-    if (editorInstanceRef.current || initializingRef.current) return;
-    initializingRef.current = true;
+    if (editorInstanceRef.current || initializingRef.current) return
+    initializingRef.current = true
 
     const defaultOperations =
-      localStorage.getItem("operations") ??
+      localStorage.getItem('operations') ??
       `
 # cmd/ctrl + return/enter will execute the op,
 # same in variables editor below
@@ -30,81 +31,86 @@ query($limit: Int!) {
         customer
     }
 }
-`;
+`
 
     const getOrCreateModel = (uri: string, value: string) => {
       return (
         editor.getModel(Uri.file(uri)) ??
-        editor.createModel(value, uri.split(".").pop(), Uri.file(uri))
-      );
-    };
+        editor.createModel(value, uri.split('.').pop(), Uri.file(uri))
+      )
+    }
 
-    const queryModel = getOrCreateModel("operation.graphql", defaultOperations);
+    const queryModel = getOrCreateModel('operation.graphql', defaultOperations)
+
+    queryModel.onDidChangeContent(
+      debounce(() => {
+        console.log('content changed', queryModel.getValue())
+      }, 600)
+    )
 
     const editorInstance = editor.create(editorContainerElementRef.current!, {
       model: queryModel,
-      language: "graphql",
-    });
+      language: 'graphql',
+    })
 
     initializeMode({
       diagnosticSettings: {
         validateVariablesJSON: {
-          [Uri.file("operation.graphql").toString()]: [
-            Uri.file("variables.json").toString(),
+          [Uri.file('operation.graphql').toString()]: [
+            Uri.file('variables.json').toString(),
           ],
         },
         jsonDiagnosticSettings: {
           validate: true,
-          schemaValidation: "error",
+          schemaValidation: 'error',
           // set these again, because we are entirely re-setting them here
           allowComments: true,
-          trailingCommas: "ignore",
+          trailingCommas: 'ignore',
         },
       },
       schemas: [
         {
           schema: await getSchema(),
-          uri: "myschema.graphql",
+          uri: 'myschema.graphql',
         },
       ],
-    });
+    })
 
-    editorInstanceRef.current = editorInstance;
-    initializingRef.current = false;
-  };
+    editorInstanceRef.current = editorInstance
+    initializingRef.current = false
+  }
 
   useEffect(() => {
-    init();
+    init()
     return () => {
       if (editorInstanceRef.current) {
-        editorInstanceRef.current.dispose();
-        editorInstanceRef.current = null;
-        initializingRef.current = false;
+        editorInstanceRef.current.dispose()
+        editorInstanceRef.current = null
+        initializingRef.current = false
       }
-    };
-  }, []);
+    }
+  }, [])
 
-  return { editorContainerElementRef };
+  return { editorContainerElementRef }
 }
 
 async function getSchema(): Promise<GraphQLSchema> {
   const fetcher = createGraphiQLFetcher({
-    url: "https://countries.trevorblades.com",
+    url: 'https://countries.trevorblades.com',
     // Add WebSocket options or disable subscriptions
-  });
+  })
 
   const data = await fetcher({
     query: getIntrospectionQuery(),
-    operationName: "IntrospectionQuery",
-  });
+    operationName: 'IntrospectionQuery',
+  })
   const introspectionJSON =
-    "data" in data && (data.data as unknown as IntrospectionQuery);
+    'data' in data && (data.data as unknown as IntrospectionQuery)
 
   if (!introspectionJSON) {
     throw new Error(
-      "this demo does not support subscriptions or http multipart yet"
-    );
+      'this demo does not support subscriptions or http multipart yet'
+    )
   }
-  return buildClientSchema(introspectionJSON);
+  return buildClientSchema(introspectionJSON)
 }
-
