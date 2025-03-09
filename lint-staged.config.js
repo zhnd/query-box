7c83bb063e90984e54ea638ba832cc5dfc17b857
zhnd <zhufind@yeah.net>
2025-03-09 14:54:45 +0800

    update Rust file formatting command to specify manifest path

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -2,5 +2,5 @@
 module.exports = {
   '*.{js,jsx,mjs,ts,tsx,mts,mdx}': ['prettier --write', 'eslint --fix'],
   '*.{json,md,css,html,yml,yaml,scss}': ['prettier --write'],
-  '*.rs': ['cargo fmt --'],
+  '*.rs': ['cargo fmt --manifest-path=./src-tauri/Cargo.toml --'],
 }
