448ce6ffacb1baddb835af1e472ad0b165064cff
zhnd <zhufind@yeah.net>
2025-03-14 22:43:46 +0800

    add z-index to TitleBar for improved layering

--- src/components/titlebar/index.tsx
+++ src/components/titlebar/index.tsx
@@ -1,7 +1,7 @@
 export function TitleBar() {
   return (
     <div
-      className="h-[28px] select-none flex items-center overflow-hidden"
+      className="h-[28px] select-none flex items-center overflow-hidden z-[9999]"
       data-tauri-drag-region
     ></div>
   )
