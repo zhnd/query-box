c3637667031b415a12c7faf63c67181bebefa768
zhnd <zhufind@yeah.net>
2025-03-22 21:58:28 +0800

    refactor EndpointList to integrate DataTableRowActions and enhance cell rendering with OverflowTooltip

--- src/query-box-app/endpoint/list/index.tsx
+++ src/query-box-app/endpoint/list/index.tsx
@@ -1,17 +1,8 @@
 import { DataTable } from '@/components/data-table'
+import { DataTableRowActions } from '@/components/data-table/data-table-row-actions'
 import { OverflowTooltip } from '@/components/overflow-tooltip'
-import { Button } from '@/components/ui/button'
-import {
-  DropdownMenu,
-  DropdownMenuContent,
-  DropdownMenuItem,
-  DropdownMenuLabel,
-  DropdownMenuSeparator,
-  DropdownMenuTrigger,
-} from '@/components/ui/dropdown-menu'
 import { Endpoint } from '@/generated/typeshare-types'
 import { ColumnDef } from '@tanstack/react-table'
-import { MoreHorizontal } from 'lucide-react'
 import { useEndpointListService } from './use-service'
 
 export function EndpointList() {
@@ -20,6 +11,7 @@ export function EndpointList() {
     {
       accessorKey: 'name',
       header: 'Name',
+      cell: ({ row }) => <OverflowTooltip text={row.original.name ?? ''} />,
     },
     {
       accessorKey: 'description',
@@ -27,53 +19,43 @@ export function EndpointList() {
       cell: ({ row }) => (
         <OverflowTooltip text={row.original.description ?? ''} />
       ),
-      meta: {
-        width: '40%',
-      },
     },
     {
       accessorKey: 'url',
       header: 'URL',
-      meta: {
-        width: '30%',
-      },
+      cell: ({ row }) => <OverflowTooltip text={row.original.url ?? ''} />,
     },
     {
       accessorKey: 'endpoint_type',
       header: 'Endpoint Type',
       meta: {
-        width: '30%',
+        className: 'w-[150px]',
       },
     },
     {
       id: 'actions',
       enableHiding: false,
-      cell: () => {
-        return (
-          <div className="w-1">
-            <DropdownMenu>
-              <DropdownMenuTrigger asChild>
-                <Button variant="ghost" className="h-8 w-8 p-0">
-                  <span className="sr-only">Open menu</span>
-                  <MoreHorizontal />
-                </Button>
-              </DropdownMenuTrigger>
-              <DropdownMenuContent align="end">
-                <DropdownMenuLabel>Actions</DropdownMenuLabel>
-                <DropdownMenuSeparator />
-                <DropdownMenuItem>Edit</DropdownMenuItem>
-                <DropdownMenuItem>Delete</DropdownMenuItem>
-              </DropdownMenuContent>
-            </DropdownMenu>
-          </div>
-        )
+      meta: {
+        className: 'w-[80px]',
       },
+      cell: () => (
+        <DataTableRowActions
+          actions={[
+            {
+              label: 'Edit',
+              onClick: () => {},
+            },
+            {
+              label: 'Delete',
+              onClick: () => {},
+            },
+          ]}
+        />
+      ),
     },
   ]
 
   return (
-    <div className="">
-      <DataTable columns={columns} data={service.endpointsInfo?.items ?? []} />
-    </div>
+    <DataTable columns={columns} data={service.endpointsInfo?.items ?? []} />
   )
 }
