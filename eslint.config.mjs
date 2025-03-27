16263f4ccf932eaed7a4c213f542a770c501a23c
zhnd <zhufind@yeah.net>
2025-03-27 22:55:45 +0800

    add eslint-plugin-query to ESLint configuration

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,42 +1,44 @@
-import pluginJs from "@eslint/js";
-import eslintConfigPrettier from "eslint-config-prettier";
-import pluginReact from "eslint-plugin-react";
-import globals from "globals";
-import tseslint from "typescript-eslint";
+import pluginJs from '@eslint/js'
+import pluginQuery from '@tanstack/eslint-plugin-query'
+import eslintConfigPrettier from 'eslint-config-prettier'
+import pluginReact from 'eslint-plugin-react'
+import globals from 'globals'
+import tseslint from 'typescript-eslint'
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
   {
     ignores: [
-      "node_modules/*",
-      "dist/*",
-      "src-tauri/*",
-      "webpack/*",
-      ".vscode/*",
-      "public/*",
-      "pnpm-lock.yaml",
+      'node_modules/*',
+      'dist/*',
+      'src-tauri/*',
+      'webpack/*',
+      '.vscode/*',
+      'public/*',
+      'pnpm-lock.yaml',
     ],
   },
   {
-    files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"],
+    files: ['**/*.{js,mjs,cjs,ts,jsx,tsx}'],
   },
 
   { languageOptions: { globals: globals.browser } },
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
   pluginReact.configs.flat.recommended,
+  ...pluginQuery.configs['flat/recommended'],
   eslintConfigPrettier,
   {
     settings: {
       react: {
-        version: "detect",
+        version: 'detect',
       },
     },
   },
   {
     rules: {
-      "react/jsx-uses-react": "off",
-      "react/react-in-jsx-scope": "off",
+      'react/jsx-uses-react': 'off',
+      'react/react-in-jsx-scope': 'off',
     },
   },
-];
+]
