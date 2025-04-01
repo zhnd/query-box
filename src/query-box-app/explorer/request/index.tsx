4bc350bcf0c8b965472ab918c0b2957792998e8f
zhnd <zhufind@yeah.net>
2025-04-01 23:19:38 +0800

    update layout of Request component for full height display

--- src/query-box-app/explorer/request/index.tsx
+++ src/query-box-app/explorer/request/index.tsx
@@ -2,7 +2,7 @@ import { QueryEditor } from '@/components/query-editor'
 
 export function Request() {
   return (
-    <div className="flex-1 flex">
+    <div className="flex h-full">
       <QueryEditor />
     </div>
   )
