ab5dc552b338a436910bfaa41574e504f5817fb8
zhnd <zhufind@yeah.net>
2025-04-02 23:10:01 +0800

    reduce height of TitleBar component

--- src/components/titlebar/index.tsx
+++ src/components/titlebar/index.tsx
@@ -1,7 +1,7 @@
 export function TitleBar() {
   return (
     <div
-      className="h-[28px] select-none flex items-center overflow-hidden z-[9999]"
+      className="h-[20px] select-none flex items-center overflow-hidden z-[9999]"
       data-tauri-drag-region
     ></div>
   )
