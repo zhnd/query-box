e431648db70fc29b187119c9365616372530bb16
zhnd <zhufind@yeah.net>
2025-06-18 14:55:15 +0800

    feat: auto focus and set app on top

--- src/query-box-app/use-app-service.ts
+++ src/query-box-app/use-app-service.ts
@@ -1,6 +1,8 @@
+import { useWindowFrontAndFocus } from '@/hooks'
 import { useAppSidebarMenuStore } from '@/stores'
 
 export const useAppService = () => {
+  useWindowFrontAndFocus()
   const { activeItemKey } = useAppSidebarMenuStore()
   return {
     activeAppSidebarMenuItem: activeItemKey,
