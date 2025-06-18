ccd4cf0d7c9322d1fe1d99cf14e609370905e87d
zhnd <zhufind@yeah.net>
2025-06-18 17:04:44 +0800

    feat: add EmptyState component and integrate it into Explorer for no endpoint selected scenario

--- src/query-box-app/explorer/use-explorer-service.ts
+++ src/query-box-app/explorer/use-explorer-service.ts
@@ -1,10 +1,16 @@
-import { useGraphQLExplorerPageStore } from '@/stores'
+import {
+  useEndpointSelectedStateStore,
+  useGraphQLExplorerPageStore,
+} from '@/stores'
 
 export const useExplorerService = () => {
   const response = useGraphQLExplorerPageStore((state) => state.response)
+  const currentPageSelectedEndpoint = useEndpointSelectedStateStore(
+    (state) => state.currentPageSelectedEndpoint
+  )
   const pageLoadState = useGraphQLExplorerPageStore(
     (state) => state.pageLoadState
   )
 
-  return { response, pageLoadState }
+  return { response, pageLoadState, currentPageSelectedEndpoint }
 }
