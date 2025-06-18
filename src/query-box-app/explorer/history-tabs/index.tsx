41809de08c4630771a3dff9013455cac5b0e03fa
zhnd <zhufind@yeah.net>
2025-06-18 19:25:34 +0800

    feat: enhance tab scrolling behavior and update tabs styles

--- src/query-box-app/explorer/history-tabs/index.tsx
+++ src/query-box-app/explorer/history-tabs/index.tsx
@@ -5,7 +5,6 @@ import {
   DropdownMenuItem,
   DropdownMenuTrigger,
 } from '@/components/ui/dropdown-menu'
-import { cn } from '@/lib/utils'
 import { ChevronRight, MoreHorizontal, Plus, X } from 'lucide-react'
 import { useRequestHistoryTabsService } from './use-service'
 
@@ -13,24 +12,24 @@ export function RequestHistoryTabs() {
   const service = useRequestHistoryTabsService()
 
   return (
-    <div className="tab-bar flex flex-row gap-2 items-center border-b">
+    <div className="tab-bar flex flex-row gap-2 items-center">
       <div
-        className="flex-1 flex space-x-4 min-w-0 overflow-x-auto scrollbar-hide"
+        className="flex-1 flex space-x-4 min-w-0 overflow-x-auto scrollbar-hide bg-muted text-muted-foreground items-center rounded-lg p-1"
         ref={service.tabsContainerRef}
       >
         {service.requestHistories.map((record, index) => (
           <div
             key={record.id}
             data-tab-id={record.id}
+            data-state={
+              service.activeRequestHistory?.id === record.id
+                ? 'active'
+                : 'inactive'
+            }
             onClick={() => {
               service.handleActiveTabChange(record.id)
             }}
-            className={cn(
-              'flex items-center shrink-0 cursor-pointer gap-2 group p-1 box-border border-b-2 border-transparent hover:border-b-primary',
-              service.activeRequestHistory?.id === record.id
-                ? 'border-b-primary'
-                : ''
-            )}
+            className="cursor-pointer data-[state=active]:bg-background data-[state=active]:text-foreground focus-visible:border-ring focus-visible:ring-ring/50 focus-visible:outline-ring inline-flex items-center justify-center gap-1.5 rounded-md px-2 py-1 text-sm font-medium whitespace-nowrap transition-[color,box-shadow] focus-visible:ring-[3px] focus-visible:outline-1 disabled:pointer-events-none disabled:opacity-50 data-[state=active]:shadow-sm [&_svg]:pointer-events-none [&_svg]:shrink-0 [&_svg:not([class*='size-'])]:size-4"
           >
             <span className="truncate flex-1 text-xs">
               {record.name ?? `Request ${index + 1}`}
@@ -47,7 +46,7 @@ export function RequestHistoryTabs() {
           </div>
         ))}
       </div>
-      <div>
+      <div className="flex items-center gap-1.5">
         <Button
           variant="ghost"
           size="icon"
