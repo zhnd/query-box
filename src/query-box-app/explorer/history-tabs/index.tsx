d00e384acabc781388a9e84258c388aeeb02a93f
zhnd <zhufind@yeah.net>
2025-06-18 23:16:06 +0800

    style: refine tab layout and improve spacing in RequestHistoryTabs component

--- src/query-box-app/explorer/history-tabs/index.tsx
+++ src/query-box-app/explorer/history-tabs/index.tsx
@@ -13,38 +13,40 @@ export function RequestHistoryTabs() {
 
   return (
     <div className="tab-bar flex flex-row gap-2 items-center">
-      <div
-        className="flex-1 flex space-x-4 min-w-0 overflow-x-auto scrollbar-hide bg-muted text-muted-foreground items-center rounded-lg p-1 scroll-px-1"
-        ref={service.tabsContainerRef}
-      >
-        {service.requestHistories.map((record, index) => (
-          <div
-            key={record.id}
-            data-tab-id={record.id}
-            data-state={
-              service.activeRequestHistory?.id === record.id
-                ? 'active'
-                : 'inactive'
-            }
-            onClick={() => {
-              service.handleActiveTabChange(record.id)
-            }}
-            className="cursor-pointer data-[state=active]:bg-background data-[state=active]:text-foreground focus-visible:border-ring focus-visible:ring-ring/50 focus-visible:outline-ring inline-flex items-center justify-center gap-1.5 rounded-md px-2 py-1 text-sm font-medium whitespace-nowrap transition-[color,box-shadow] focus-visible:ring-[3px] focus-visible:outline-1 disabled:pointer-events-none disabled:opacity-50 data-[state=active]:shadow-sm [&_svg]:pointer-events-none [&_svg]:shrink-0 [&_svg:not([class*='size-'])]:size-4"
-          >
-            <span className="truncate flex-1 text-xs">
-              {record.name ?? `Request ${index + 1}`}
-            </span>
-            <Button
-              variant="ghost"
-              size="icon"
-              className="flex items-center cursor-pointer w-5 h-5"
-              onClick={() => service.handleDeleteRequestHistory(record.id)}
+      <div className="flex-1 flex min-w-0 px-2 bg-muted rounded-lg">
+        <div
+          className="flex-1 flex space-x-2 min-w-0 overflow-x-auto scrollbar-hide text-muted-foreground items-center p-1 scroll-px-1"
+          ref={service.tabsContainerRef}
+        >
+          {service.requestHistories.map((record, index) => (
+            <div
+              key={record.id}
+              data-tab-id={record.id}
+              data-state={
+                service.activeRequestHistory?.id === record.id
+                  ? 'active'
+                  : 'inactive'
+              }
+              onClick={() => {
+                service.handleActiveTabChange(record.id)
+              }}
+              className="cursor-pointer data-[state=active]:bg-background data-[state=active]:text-foreground focus-visible:border-ring focus-visible:ring-ring/50 focus-visible:outline-ring inline-flex items-center justify-center gap-1.5 rounded-md px-2 py-1 text-sm font-medium whitespace-nowrap transition-[color,box-shadow] focus-visible:ring-[3px] focus-visible:outline-1 disabled:pointer-events-none disabled:opacity-50 data-[state=active]:shadow-sm [&_svg]:pointer-events-none [&_svg]:shrink-0 [&_svg:not([class*='size-'])]:size-4"
             >
-              <X />
-              <span className="sr-only">Close tab</span>
-            </Button>
-          </div>
-        ))}
+              <span className="truncate flex-1 text-xs">
+                {record.name ?? `Request ${index + 1}`}
+              </span>
+              <Button
+                variant="ghost"
+                size="icon"
+                className="flex items-center cursor-pointer w-5 h-5"
+                onClick={() => service.handleDeleteRequestHistory(record.id)}
+              >
+                <X />
+                <span className="sr-only">Close tab</span>
+              </Button>
+            </div>
+          ))}
+        </div>
       </div>
       <div className="flex items-center gap-1.5">
         <Button
